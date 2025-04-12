d6bb23ae2ac5b9f24a946ab17342921d56ec9f64
roshan129 <roshanadke2015@gmail.com>
2025-04-12 11:48:28 +0530

    initial commit

---
+++ app/src/main/java/com/roshanadke/drawcanvascompose/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.roshanadke.drawcanvascompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.roshanadke.drawcanvascompose.ui.theme.DrawCanvasComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DrawCanvasComposeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Column(modifier = Modifier.padding(innerPadding)) {  }
+                }
+            }
+        }
+    }
+}
